4a10248a76cb05d20bb99ea332bd3264e6b928c8
Vinicius Guedes <viniciusgued@gmail.com>
2018-02-15 18:42:10 -0200

    Updates some dependencies and adds main packages to types file

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,10 @@ import { StackerJS } from 'stackerjs-types';
 declare module "stackerjs-http"
 {
 
-    
+    export class Request extends StackerJS.Http.Request {}
+
+    export class Response extends StackerJS.Http.Response {}
+
+    export class MakeRequest extends StackerJS.Http.MakeRequest {}
 
 }
