caaa07143d410c9480da3ad0b2951f3fa9487b30
Vinicius Guedes <viniciusgued@gmail.com>
2018-02-20 12:06:22 -0300

    features: Adds getMessage() to exceptions

--- src/Exception/HttpError.js
+++ src/Exception/HttpError.js
@@ -6,11 +6,16 @@ export class HttpError extends Error
 
     constructor(message)
     {
-        super(message);
+        super(typeof message === 'object' ? JSON.stringify(message) : message);
         this.code = Response.HTTP_INTERNAL_SERVER_ERROR;
-        this.message = message;
 
         this.getCode = () => this.code;
+        this.getMessage = () =>
+        {
+            try {
+                return JSON.parse(this.message);
+            } catch (err) { return this.message }
+        }
     }
 
 }
--- test/Unit/Exceptions.test.js
+++ test/Unit/Exceptions.test.js
@@ -6,9 +6,10 @@ describe('Unit/ExceptionsTest', () =>
 {
     it('Should throw BadRequestException', done => 
     {
-        Promise.reject(new Http.Exception.BadRequestError("bad"))
+        Promise.reject(new Http.Exception.BadRequestError({ 'status': false }))
             .catch(err => {
-                expect(() => { throw err }).to.throw('bad');
+                expect(err.getMessage().status).to.be.false;
+                expect(() => { throw err }).to.throw();
                 expect(err.getCode()).to.be.equal(Http.Response.HTTP_BAD_REQUEST);
             })
             .then(() => done());
@@ -18,6 +19,7 @@ describe('Unit/ExceptionsTest', () =>
     {
         Promise.reject(new Http.Exception.UnauthorizedError("Unauthorized"))
             .catch(err => {
+                expect(err.getMessage()).to.be.equal('Unauthorized');
                 expect(() => { throw err }).to.throw('Unauthorized');
                 expect(err.getCode()).to.be.equal(Http.Response.HTTP_UNAUTHORIZED);
             })
@@ -34,7 +36,7 @@ describe('Unit/ExceptionsTest', () =>
             .then(() => done());
     });
 
-    it('Should throw NotFoundException', () => 
+    it('Should throw NotFoundException', done => 
     {
         Promise.reject(new Http.Exception.NotFoundError("Not found"))
             .catch(err => {
