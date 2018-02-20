97164f0875bea10ea2a93a9a4a810be19ed349f7
Vinicius Guedes <viniciusgued@gmail.com>
2018-02-20 12:32:32 -0300

    fix: Now Exceptions and fully represented by HttpError class

--- src/Exception/HttpError.js
+++ src/Exception/HttpError.js
@@ -7,8 +7,9 @@ export class HttpError extends Error
     constructor(message)
     {
         super(typeof message === 'object' ? JSON.stringify(message) : message);
-        this.code = Response.HTTP_INTERNAL_SERVER_ERROR;
+        Error.captureStackTrace(this, HttpError);
 
+        this.code = Response.HTTP_INTERNAL_SERVER_ERROR;
         this.getCode = () => this.code;
         this.getMessage = () =>
         {
